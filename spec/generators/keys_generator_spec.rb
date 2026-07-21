d1695ba2e1648cfbfa66354a4e73304a23d27394
wafihfzhn <hafizhuddin96@gmail.com>
2026-07-21 11:52:25 +0800

    Fix install and keys generator spec

--- spec/generators/keys_generator_spec.rb
+++ spec/generators/keys_generator_spec.rb
@@ -20,39 +20,39 @@ RSpec.describe JwksProvider::Generators::KeysGenerator do
   after { FileUtils.rm_rf(tmpdir) }
 
   describe "#generate_key_pairs" do
-    it "creates enc_key.pem and sig_key.pem for staging and production" do
+    it "creates enc_key and sig_key PEM files for stg and prd" do
       run_keys_generator(tmpdir)
 
-      %w[staging production].each do |env|
-        expect(File).to exist(File.join(tmpdir, "config/keys/#{env}/enc_key.pem"))
-        expect(File).to exist(File.join(tmpdir, "config/keys/#{env}/sig_key.pem"))
+      %w[stg prd].each do |env|
+        expect(File).to exist(File.join(tmpdir, "config/keys/enc_key_#{env}.pem"))
+        expect(File).to exist(File.join(tmpdir, "config/keys/sig_key_#{env}.pem"))
       end
     end
 
-    it "generates valid EC PEM content in enc_key.pem" do
+    it "generates valid EC PEM content in enc_key PEM files" do
       run_keys_generator(tmpdir)
 
-      %w[staging production].each do |env|
-        pem = File.read(File.join(tmpdir, "config/keys/#{env}/enc_key.pem"))
+      %w[stg prd].each do |env|
+        pem = File.read(File.join(tmpdir, "config/keys/enc_key_#{env}.pem"))
         expect { OpenSSL::PKey::EC.new(pem) }.not_to raise_error
       end
     end
 
-    it "skips generation if enc_key.pem already exists" do
-      dir = File.join(tmpdir, "config/keys/staging")
+    it "skips generation if enc_key_stg.pem already exists" do
+      dir = File.join(tmpdir, "config/keys")
       FileUtils.mkdir_p(dir)
       existing_key = OpenSSL::PKey::EC.generate("prime256v1").to_pem
-      File.write(File.join(dir, "enc_key.pem"), existing_key)
+      File.write(File.join(dir, "enc_key_stg.pem"), existing_key)
 
       run_keys_generator(tmpdir)
 
-      expect(File.read(File.join(dir, "enc_key.pem"))).to eq(existing_key)
+      expect(File.read(File.join(dir, "enc_key_stg.pem"))).to eq(existing_key)
     end
 
     it "generates a prime256v1 EC key" do
       run_keys_generator(tmpdir)
 
-      pem = File.read(File.join(tmpdir, "config/keys/staging/enc_key.pem"))
+      pem = File.read(File.join(tmpdir, "config/keys/enc_key_stg.pem"))
       key = OpenSSL::PKey::EC.new(pem)
       expect(key.group.curve_name).to eq("prime256v1")
     end
